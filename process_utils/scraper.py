4ffdcc8f5247ae4230c8d53b097ba01998a45eb7
Connor Keefe <connorkeefe99@gmail.com>
2024-12-26 12:40:11 -0400

    Scraper fixes

--- process_utils/scraper.py
+++ process_utils/scraper.py
@@ -21,10 +21,27 @@ def detect_date_format_credit(s):
     # - (?:3[01]|[12][0-9]|0?[1-9]): Non-capturing group for dates: 01-31, allowing for a leading zero or not.
     # - The entire month and date pattern is repeated twice, with a space in between.
     pattern = r"(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\. (?:3[01]|[12][0-9]|0?[1-9]) (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\. (?:3[01]|[12][0-9]|0?[1-9])"
-    
+
     # Search for the pattern in the string
     match = re.search(pattern, s)
-    
+
+    # If a match is found, return True; otherwise, False
+    return bool(match)
+
+
+
+def detect_date_format_credit_single(s):
+    # Adjusted regex pattern explanation:
+    # - ^: Matches the start of the string.
+    # - (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\.: Matches abbreviated month names followed by a period.
+    # - \s+: Matches one or more whitespace characters.
+    # - (?:3[01]|[12][0-9]|0?[1-9]): Non-capturing group for dates: 01-31, allowing for a leading zero or not.
+    # - \s*$: Matches any number of whitespace characters until the end of the string.
+    pattern = r"^(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\. \s*(?:3[01]|[12][0-9]|0?[1-9])\s*$"
+
+    # Search for the pattern in the string
+    match = re.search(pattern, s.strip())
+
     # If a match is found, return True; otherwise, False
     return bool(match)
 
@@ -72,6 +89,19 @@ def starts_with_digit_rbc(s):
     # Use startswith() to check if the string starts with any digit
     return s.isdigit()
 
+def preprocess_lines(lines):
+    new_lines = []
+    for ind, line in enumerate(lines):
+        if ind + 1 <= len(lines):
+            if detect_date_format_credit_single(line) and detect_date_format_credit_single(lines[ind + 1]):
+                new_lines.append(line + lines[ind + 1])
+        if detect_date_format_credit_single(line):
+            continue
+        else:
+            new_lines.append(line)
+    return new_lines
+
+
 
 def parse_transactions_credit(f):
     # Assuming each line contains a transaction: Date, Description, Amount
@@ -79,6 +109,7 @@ def parse_transactions_credit(f):
     text = extract_text_from_pdf(f)
     data = []
     lines = text.split('\n')
+    lines = preprocess_lines(lines)
     for i, line in enumerate(lines):
         # print(line)
         # print(detect_date_format(line))
@@ -181,10 +212,68 @@ def parse_transactions_bank_rbc(f):
     df = pd.DataFrame(data, columns=['Date', 'Description', 'Amount'])
     return df
 
+def parse_combined_bmo(f):
+    # Assuming each line contains a transaction: Date, Description, Amount
+    # Adjust the parsing logic based on your statement's format
+    chequing_account = False
+    saving_account = False
+    chequing_df = None
+    saving_df = None
+    text = extract_text_from_pdf(f)
+    data = []
+    lines = text.split('\n')
+    previous_balance = None
+    current_bal = None
+    for i, line in enumerate(lines):
+        # print(line)
+        # print(detect_date_format_bank(line))
+        if 'Chequing' in line:
+            chequing_account = True
+            saving_account = False
+        if 'Savings' in line:
+            saving_account = True
+            chequing_account = False
+        isdate = detect_date_format_bank(line)
+        if isdate and (i < (len(lines) - 3)):
+            date = line
+            description = lines[i + 1]
+            amount = None
+            if description == 'Opening balance' or description == 'Opening Balance':
+                previous_balance = lines[i + 2]
+                continue
+            if description == 'Closing totals' or description == 'Closing Balance':
+                if chequing_account:
+                    chequing_df = pd.DataFrame(data, columns=['Date', 'Description', 'Amount'])
+                    data = []
+                if saving_account:
+                    saving_df = pd.DataFrame(data, columns=['Date', 'Description', 'Amount'])
+                    data = []
+                continue
+            if starts_with_digit(lines[i + 2]) and starts_with_digit(lines[i + 3]):
+                amount = lines[i + 2]
+                current_bal = lines[i + 3]
+                if float(current_bal.replace(',', '')) > float(previous_balance.replace(',', '')):
+                    amount = amount + ' CR'
+                previous_balance = current_bal
+            if starts_with_digit(lines[i + 3]) and starts_with_digit(lines[i + 4]) and previous_balance != None:
+                description = description + lines[i + 2]
+                amount = lines[i + 3]
+                current_bal = lines[i + 4]
+                if float(current_bal.replace(',', '')) > float(previous_balance.replace(',', '')):
+                    amount = amount + ' CR'
+                previous_balance = current_bal
+            if amount != None:
+                data.append([date, description, amount])
+
+    # Convert to DataFrame for easier handling
+    # df = pd.DataFrame(data, columns=['Date', 'Description', 'Amount'])
+    return chequing_df, saving_df
+
 function_map = {
     "Credit Cards": parse_transactions_credit,
     "Bank Account": parse_transactions_bank,
-    "RBC": parse_transactions_bank_rbc
+    "RBC": parse_transactions_bank_rbc,
+    "Combined Statement": parse_combined_bmo
 }
 
 def scraper(statementPath):
@@ -198,10 +287,27 @@ def scraper(statementPath):
         funname = grandparent_name
         if 'RBC' in parent_name:
             funname = 'RBC'
+        if 'Comb' in parent_name:
+            funname = 'Combined Statement'
         fname = os.path.basename(f).replace('.pdf','.csv')
         function = function_map.get(funname)
-        output_file_name = f'data/csvs/{grandparent_name}/{parent_name}/{fname}'
-        if not(os.path.exists(output_file_name)):
-            transactions_df = function(f)
-            transactions_df.to_csv(output_file_name, index=False)
-            print(f"Scraped file saved as {output_file_name}")
+        if funname == 'Combined Statement':
+            output_chequing_name = f'data/csvs/{grandparent_name}/BMO Chq/{fname}'
+            output_saving_name = f'data/csvs/{grandparent_name}/BMO Sav/{fname}'
+            if not(os.path.exists(os.path.dirname(output_chequing_name))):
+                os.makedirs(os.path.dirname(output_chequing_name))
+            if not(os.path.exists(os.path.dirname(output_saving_name))):
+                os.makedirs(os.path.dirname(output_saving_name))
+            if not (os.path.exists(output_chequing_name)):
+                chequing_df, saving_df = function(f)
+                chequing_df.to_csv(output_chequing_name, index=False)
+                saving_df.to_csv(output_saving_name, index=False)
+                print(f"Scraped files saved as {output_saving_name} and {output_chequing_name}")
+        else:
+            output_file_name = f'data/csvs/{grandparent_name}/{parent_name}/{fname}'
+            if not(os.path.exists(os.path.dirname(output_file_name))):
+                os.makedirs(os.path.dirname(output_file_name))
+            if not(os.path.exists(output_file_name)):
+                transactions_df = function(f)
+                transactions_df.to_csv(output_file_name, index=False)
+                print(f"Scraped file saved as {output_file_name}")
